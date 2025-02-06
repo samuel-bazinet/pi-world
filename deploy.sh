407f36eeb3fd12fc9b09f60686144ca329f9dc44
samuel-bazinet <bazinet.samuel1@gmail.com>
2025-02-05 19:25:36 -0500

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly TARGET_HOST=pi@raspberrypi
+readonly TARGET_PATH=/home/pi/pi-world
+readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
+readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/pi-world
+
+cargo build --release --target=${TARGET_ARCH}
+rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+
+fn main() {
+    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Wrong number of arguments, please only provide a single numerical argument");
+    }
+
+    if let Ok(top) = args[1].parse::<usize>() {
+        println!("Printing the max prime number under {top}");
+        println!("The max prime number is {}", get_max_prime(top));
+    } else {
+        panic!("Please provide a positive integer as your single argument");
+    }
+}
+
+fn get_max_prime(max: usize) -> usize {
+    let mut primes = vec![];
+    for i in 2..=max {
+        let mut is_prime = true;
+        for &prime in &primes {
+            if i % prime == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+        if is_prime {
+            primes.push(i);
+        }
+    }
+    primes.last().copied().unwrap()
+}
